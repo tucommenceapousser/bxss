402d85271848cbf8efe744c3db22a55283893143
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-02-15 06:52:41 +0100

    Update watg.js

--- watg.js
+++ watg.js
@@ -1,5 +1,5 @@
 function openWhatsAppLink() {
-    var whatsappLink = 'https://api.whatsapp.com/send/?phone=18056009130&text=YourMessageHere&type=phone_number&app_absent=0';
+    var whatsappLink = 'https://api.whatsapp.com/send/?phone=+18056009130&text=YourMessageHere&type=phone_number';
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=Opening+WhatsApp+Link', true);
     xhr.send();
